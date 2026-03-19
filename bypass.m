9bdbb3c2f7d2e93d90467325559ad022ffd3f660
jiubana <1964315237@qq.com>
2026-03-20 02:55:22 +0800

    Bypass v2.0: use NSURLProtocol for global interception

--- bypass.m
+++ bypass.m
@@ -7,75 +7,85 @@
  * 目标：无需小火箭，直接注入游戏即可使用
  */
 
-@interface EYSBypassHook : NSObject
+/**
+ * 鹅鸭杀全自动验证绕过插件 v2.0 (NSURLProtocol 版)
+ * 特点：兼容性极强，支持 NSURLSession/NSURLConnection/WebView
+ */
+
+@interface EYSBypassProtocol : NSURLProtocol
 @end
 
-@implementation EYSBypassHook
+@implementation EYSBypassProtocol
+
++ (BOOL)canInitWithRequest:(NSURLRequest *)request {
+    NSString *url = request.URL.absoluteString;
+    // 只拦截验证服务器相关的请求
+    if ([url containsString:@"qunhongtech.com/auth/"]) {
+        return YES;
+    }
+    return NO;
+}
+
++ (NSURLRequest *)canonicalRequestForRequest:(NSURLRequest *)request {
+    return request;
+}
+
+- (void)startLoading {
+    NSString *url = self.request.URL.absoluteString;
+    NSData *mockData = nil;
+    NSString *contentType = @"application/json";
+
+    if ([url containsString:@"Notice"]) {
+        mockData = [@"{\"code\":1,\"message\":\"success\",\"data\":{\"notice\":0}}" dataUsingEncoding:NSUTF8StringEncoding];
+    } else {
+        // Base64 Success Payload
+        mockData = [@"eyJjb2RlIjoxLCJkYXRhIjp7ImFsZXJ0IjowLCJlbmR0aXAiOjAsImhpbnQiOiIiLCJ2ZXJpZnkiOjEsImp1bXAiOjB9LCJtZXNzYWdlIjoic3VjY2VzcyJ9" dataUsingEncoding:NSUTF8StringEncoding];
+        contentType = @"text/plain";
+    }
+
+    NSHTTPURLResponse *response = [[NSHTTPURLResponse alloc] initWithURL:self.request.URL
+                                                              statusCode:200
+                                                             HTTPVersion:@"HTTP/1.1"
+                                                            headerFields:@{@"Content-Type": contentType, @"Access-Control-Allow-Origin": @"*"}];
+
+    [self.client URLProtocol:self didReceiveResponse:response cacheStoragePolicy:NSURLCacheStorageNotAllowed];
+    [self.client URLProtocol:self didLoadData:mockData];
+    [self.client URLProtocolDidFinishLoading:self];
+}
+
+- (void)stopLoading {}
+
+@end
+
+// 注册插件
+@interface EYSLoader : NSObject
+@end
+
+@implementation EYSLoader
 
 + (void)load {
     static dispatch_once_t onceToken;
     dispatch_once(&onceToken, ^{
-        NSLog(@"[EYS-Bypass] 正在启动安全拦截引擎...");
-        [self eys_hookNSURLSession];
+        NSLog(@"[EYS-Bypass] 正在启动全局协议引擎...");
+        [NSURLProtocol registerClass:[EYSBypassProtocol class]];
+        [self swizzleSessionConfiguration];
     });
 }
 
-+ (void)eys_hookNSURLSession {
-    Class sessionClass = [NSURLSession class];
-    SEL originalSelector = @selector(dataTaskWithRequest:completionHandler:);
-    SEL swizzledSelector = @selector(eys_dataTaskWithRequest:completionHandler:);
-
-    Method originalMethod = class_getInstanceMethod(sessionClass, originalSelector);
-    Method swizzledMethod = class_getInstanceMethod(self, swizzledSelector);
-
-    if (class_addMethod(sessionClass, swizzledSelector, method_getImplementation(originalMethod), method_getTypeEncoding(originalMethod))) {
-        class_replaceMethod(sessionClass, originalSelector, method_getImplementation(swizzledMethod), method_getTypeEncoding(swizzledMethod));
-    } else {
-        method_exchangeImplementations(originalMethod, swizzledMethod);
-    }
++ (void)swizzleSessionConfiguration {
+    // 强制让所有 NSURLSession 也使用我们的自定义协议
+    Class cls = [NSURLSessionConfiguration class];
+    Method m1 = class_getClassMethod(cls, @selector(defaultSessionConfiguration));
+    Method m2 = class_getClassMethod([self class], @selector(eys_defaultSessionConfiguration));
+    method_exchangeImplementations(m1, m2);
 }
 
-- (NSURLSessionDataTask *)eys_dataTaskWithRequest:(NSURLRequest *)request completionHandler:(void (^)(NSData * _Nullable data, NSURLResponse * _Nullable response, NSError * _Nullable error))completionHandler {
-    
-    NSString *url = request.URL.absoluteString;
-    
-    // 拦截 鹅鸭杀/群鸿 验证接口
-    if ([url containsString:@"qunhongtech.com/auth/"]) {
-        NSLog(@"[EYS-Bypass] 拦截并模拟成功响应: %@", url);
-        
-        // 模拟延迟回调，避免“秒回”导致的游戏逻辑异常
-        dispatch_after(dispatch_time(DISPATCH_TIME_NOW, (int64_t)(0.2 * NSEC_PER_SEC)), dispatch_get_main_queue(), ^{
-            NSData *mockData = nil;
-            NSString *contentType = @"application/json";
-            
-            if ([url containsString:@"Notice"]) {
-                mockData = [@"{\"code\":1,\"message\":\"success\",\"data\":{\"notice\":0}}" dataUsingEncoding:NSUTF8StringEncoding];
-            } else {
-                // Base64 Success Payload
-                mockData = [@"eyJjb2RlIjoxLCJkYXRhIjp7ImFsZXJ0IjowLCJlbmR0aXAiOjAsImhpbnQiOiIiLCJ2ZXJpZnkiOjEsImp1bXAiOjB9LCJtZXNzYWdlIjoic3VjY2VzcyJ9" dataUsingEncoding:NSUTF8StringEncoding];
-                contentType = @"text/plain";
-            }
-            
-            NSHTTPURLResponse *mockResponse = [[NSHTTPURLResponse alloc] initWithURL:request.URL 
-                                                                        statusCode:200 
-                                                                       HTTPVersion:@"HTTP/1.1" 
-                                                                      headerFields:@{@"Content-Type": contentType, @"Access-Control-Allow-Origin": @"*"}];
-            
-            if (completionHandler) {
-                completionHandler(mockData, mockResponse, nil);
-            }
-        });
-        
-        /**
-         * 【关键修复】防止闪退
-         * 我们不能直接返回 nil，因为游戏可能对返回的 Task 进行了引用计数或状态检查。
-         * 我们调用原函数但传入空的 completion，这样既满足了返回值要求，又不会真正执行多余的回调。
-         */
-        return [self eys_dataTaskWithRequest:request completionHandler:nil]; 
-    }
-    
-    // 正常请求走原逻辑
-    return [self eys_dataTaskWithRequest:request completionHandler:completionHandler];
++ (NSURLSessionConfiguration *)eys_defaultSessionConfiguration {
+    NSURLSessionConfiguration *config = [self eys_defaultSessionConfiguration];
+    NSMutableArray *protocols = [NSMutableArray arrayWithArray:config.protocolClasses];
+    [protocols insertObject:[EYSBypassProtocol class] atIndex:0];
+    config.protocolClasses = protocols;
+    return config;
 }
 
 @end
